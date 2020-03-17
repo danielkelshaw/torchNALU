5fafe63586438332cd8bb2fb188700b94992fed0
Daniel Kelshaw <daniel.j.kelshaw@gmail.com>
2020-03-17 21:06:38 +0000

    Moved models to new directory

---
+++ models/__init__.py
@@ -0,0 +1,5 @@
+from .mlp import MLP
+from .nac import NeuralAccumulatorCell
+from .nalu import NALU
+
+__all__ = ['mlp', 'nac', 'nalu']
---
+++ network.py
@@ -0,0 +1,18 @@
+import argparse
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+def train():
+    pass
+
+
+def test():
+    pass
+
+
+def main():
+    pass
